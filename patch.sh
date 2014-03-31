f8b3d1997d78b67ea888827820bddb41e7360ac3
daothanhduy <daothanhduy2806@gmail.com>
2014-03-31 14:25:04 +0700

    Make it into a script file to ensure it will work.

---
+++ patch.sh
@@ -0,0 +1,5 @@
+cp patches/bionic/libc/Android.mk bionic/libc/Android.mk
+cp patches/system/core/charger/charger.c system/core/charger/charger.c
+cp patches/build/core/Makefile build/core/Makefile
+cp patches/build/tools/releasetools/common.py build/tools/releasetools/common.py
+cp patches/local.xml .repo/local_manifests/local.xml
